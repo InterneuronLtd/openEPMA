c39783c3943856060e409461f0475a3e758fd1cc
Jenkins <jenkins@ip-10-0-1-82.ap-south-1.compute.internal>
2024-08-07 09:33:43 +0000

    Release v1.15

--- src/app/components/warnings-mod/warnings-mod.component.spec.ts
+++ src/app/components/warnings-mod/warnings-mod.component.spec.ts
@@ -1,7 +1,7 @@
 //BEGIN LICENSE BLOCK 
 //Interneuron Terminus
 
-//Copyright(C) 2024  Interneuron Holdings Ltd
+//Copyright(C) 2024  Interneuron Limited
 
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
